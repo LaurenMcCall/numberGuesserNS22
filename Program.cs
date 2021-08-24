61fb6dc1b6572e72bd259f4d3160179935199319
Lauren McCall <lmmccolloch@gmail.com>
2021-08-23 20:40:28 -0400

    Number Guesser

--- Program.cs
+++ Program.cs
@@ -2,11 +2,92 @@
 
 namespace NumberGuesser
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            ShowGreeting();
+
+
+            int low = 1;
+            int high = 1024;
+            int guess = 0;
+            string userResponse = "";
+
+            while (userResponse != "correct")
+            {
+                // Computer makes a guess
+                // low =1 and high = 3 ... middle = 1 + 3 / 2 = 2
+                // guess = 512
+                guess = (low + high) / 2;
+                userResponse = PromptGuessCorrectness(guess);
+                // Console.WriteLine($"Your number is {guess}");
+
+                // We should check for the validity of the userResponse
+                if (!ValidateResponse(userResponse))
+                {
+                    Console.WriteLine("invalid response. exiting the game...");
+                    return;
+
+                }
+
+
+                // Computer prompts for a response
+                // Console.WriteLine($"Is your number LOWER, HIGHER, or CORRECT?");
+                // userResponse = Console.ReadLine();
+                // userResponse = userResponse.ToLower();
+
+                // If the answer is incorrect, we want to redefine low or high to make
+                // a better subsequent guess
+                if (userResponse == "lower")
+                {
+                    high = guess;
+                }
+                else if (userResponse == "higher")
+                {
+                    low = guess;
+                }
+            }
+
+        }
+
+        static void ShowGreeting()
+        {
+            Console.WriteLine("Welcome to Number Guesser");
+            Console.WriteLine("Think of a number between 1 and 1024");
+            Console.WriteLine("I will try to guess the number ...");
+        }
+
+        static string PromptGuessCorrectness(int guess)
+        {
+            // Computer prompt for number
+            Console.WriteLine($"Is your number LOWER, HIGHER, or CORRECT?");
+            return Console.ReadLine().ToLower();
+
+            // userResponse = Console.ReadLine();
+            // userResponse = userResponse.ToLower();
+        }
+
+        static bool ValidateResponse(string userResponse);
+        {
+            if (userResponse == "correct" || userResponse == "higher" || userResponse == "lower")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    static void BragWhenCorrect()
+    {
+        Console.WriteLine("MWAHAHA I am UNDEFEATED!");
     }
-  }
+
+    // ShowGreeting 
+    // ComputeNewLowIfTooLow
+    // ComputeNewHighIfTooHigh
+    // BragWhenCorrect
+}
 }
